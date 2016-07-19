d3449701b28ffcd9d1e1290308a1079772815ef8
kkotov <kkotov@cern.ch>
2016-07-19 17:58:13 +0200

    L1T Menu only O2O branch for 81X

--- CondCore/L1TPlugins/src/plugin.cc
+++ CondCore/L1TPlugins/src/plugin.cc
@@ -251,7 +251,9 @@ REGISTER_PLUGIN(L1TUtmScaleRcd, L1TUtmScale);
 
 #include "CondFormats/L1TObjects/interface/L1TUtmTriggerMenu.h"
 #include "CondFormats/DataRecord/interface/L1TUtmTriggerMenuRcd.h"
+#include "CondFormats/DataRecord/interface/L1TUtmTriggerMenuO2ORcd.h"
 REGISTER_PLUGIN(L1TUtmTriggerMenuRcd, L1TUtmTriggerMenu);
+REGISTER_PLUGIN(L1TUtmTriggerMenuO2ORcd, L1TUtmTriggerMenu);
 
 #include "CondFormats/L1TObjects/interface/L1TGlobalParameters.h"
 #include "CondFormats/DataRecord/interface/L1TGlobalParametersRcd.h"
