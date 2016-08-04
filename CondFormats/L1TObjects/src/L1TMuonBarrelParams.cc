32ad8ce3f31878bc8c0b7756fd49d45b6e889d02
kkotov <kkotov@cern.ch>
2016-08-04 18:11:01 +0200

    revert L1TMuonBarrelParams back

--- CondFormats/L1TObjects/src/L1TMuonBarrelParams.cc
+++ CondFormats/L1TObjects/src/L1TMuonBarrelParams.cc
@@ -9,3 +9,10 @@ L1TMuonBarrelParams::L1TMuonBarrelParams() :
   pnodes_[CONFIG].iparams_.resize(NUM_CONFIG_PARAMS);
   version_=Version;
 }
+
+void L1TMuonBarrelParams::print(std::ostream& out) const {
+
+  out << "L1 BMTF Parameters" << std::endl;
+
+  out << "Firmware version: " << fwVersion_ << std::endl;
+}
